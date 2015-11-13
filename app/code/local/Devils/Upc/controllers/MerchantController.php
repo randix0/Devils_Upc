102e35031cc53d74b2ae562225e2769680d3a51e
Yuriy Denyshchenko <ydenyshchenk@ebay.com>
2015-11-13 23:41:41 +0200

    notify test

--- app/code/local/Devils/Upc/controllers/MerchantController.php
+++ app/code/local/Devils/Upc/controllers/MerchantController.php
@@ -111,11 +111,11 @@ class Devils_Upc_MerchantController extends Mage_Core_Controller_Front_Action
                 'XID', 'PurchaseTime', 'Response.action', 'Response.reason', 'Response.forwardUrl');
             $data['Response.action'] = 'reverse';
             $data['Response.reason'] = '';
-            $data['Response.forwardUrl'] = Mage::getBaseUrl('upc/merchant/failure');
+            $data['Response.forwardUrl'] = Mage::getBaseUrl('upc/merchant/failure/');
 
             if ($paymentStatus) {
                 $data['Response.action'] = 'approve';
-                $data['Response.forwardUrl'] = Mage::getBaseUrl('upc/merchant/success');
+                $data['Response.forwardUrl'] = Mage::getBaseUrl('upc/merchant/success/');
             }
             Mage::log('notifyAction: ' . json_encode($data), null, 'upc.log', true);
 
